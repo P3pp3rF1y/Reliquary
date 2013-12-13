738cf8db0cf0dcd9c8d16ae8a6b5e3c98138451b
TheMike <zeusintraining@gmail.com>
2013-12-12 21:11:06 -0500

    Messed around with Wraith's Eye.
    I've been fixing the Wraith's Eye code up some. Some issues I changed:
    
    - Added all of the improvements x3n0 had in one of his commits, since I
    started with his initial source build (1.0).
    
    - In the old Eye, you would lose your link if you entered another
    dimension. Now it will just spit back ‘Out of Range’ at you.
    
    - The tooltip world call never really worked, since WorldClient only
    read it half the time. It is now connected until you try to teleport,
    where it will notify you that the node doesn't exist and it will reset
    itself.
    
    - Why was getShort() being called, and then being checked for zero? I
    changed them to Integer (yes, I wrote an auto conversion function for
    old worlds), and used hasKey(). This makes it so Wraith Nodes work
    where X, Y, or Z is zero.
    
    - Particles! And sounds! Makes the experience so much more lively.
    
    As usual, test, test, test.

--- src/main/java/xreliquary/items/ItemWraithEye.java
+++ src/main/java/xreliquary/items/ItemWraithEye.java
@@ -1,11 +1,16 @@
 package xreliquary.items;
 
+import java.util.Iterator;
 import java.util.List;
 
 import net.minecraft.client.renderer.texture.IconRegister;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTBase;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.ChatMessageComponent;
 import net.minecraft.util.EnumChatFormatting;
 import net.minecraft.util.Icon;
 import net.minecraft.world.World;
@@ -72,19 +77,53 @@ public class ItemWraithEye extends ItemSalamanderEye {
     }
 
     @Override
-    public ItemStack onItemRightClick(ItemStack ist, World world,
-            EntityPlayer player) {
-        // teleport player to the wraith node, if it's possible to.
-        if (world.getBlockId(getShort("nodeX" + getWorld(player), ist), getShort("nodeY" + getWorld(player), ist),
-                getShort("nodeZ" + getWorld(player), ist)) == XRBlocks.wraithNode.blockID) {
-            if (findAndRemoveEnderPearl(player)) {
-                attemptTeleport(world, getShort("nodeX" + getWorld(player), ist),
-                        getShort("nodeY" + getWorld(player), ist), getShort("nodeZ" + getWorld(player), ist), player);
+    public void onUpdate(ItemStack ist, World world, Entity e, int i, boolean f) {
+        //make sure to call the Salamander's Eye update function or it loses its inheritance.
+        super.onUpdate(ist, world, e, i, f);
+        
+        //checks to see if cooldown variable > 0 and decrements if true, each tick.
+        decrementCooldown(ist);        
+    }
+    
+    @Override
+    public ItemStack onItemRightClick(ItemStack eye, World world, EntityPlayer par2EntityPlayer) {
+    	checkForOldVersions(eye);
+    	
+        if (this.getShort("cooldown", eye) > 0)
+            return eye;
+        
+        if (eye.getTagCompound() != null && eye.getTagCompound().getInteger("dimensionID") != Integer.valueOf(getWorld(par2EntityPlayer))) {
+        	if(!world.isRemote) {
+        		par2EntityPlayer.sendChatToPlayer(ChatMessageComponent.createFromText(EnumChatFormatting.DARK_RED + "Out of range!"));
+        	}
+        } else if (eye.getTagCompound() != null && world.getBlockId(eye.getTagCompound().getInteger("nodeX" + getWorld(par2EntityPlayer)), eye.getTagCompound().getInteger("nodeY" + getWorld(par2EntityPlayer)), eye.getTagCompound().getInteger("nodeZ" + getWorld(par2EntityPlayer))) == XRBlocks.wraithNode.blockID) {
+            
+        	if (canTeleport(world, eye.getTagCompound().getInteger("nodeX" + getWorld(par2EntityPlayer)), eye.getTagCompound().getInteger("nodeY" + getWorld(par2EntityPlayer)), eye.getTagCompound().getInteger("nodeZ" + getWorld(par2EntityPlayer)))) {
+
+        		if (findAndRemoveEnderPearl(par2EntityPlayer)) {
+                    teleportPlayer(world, eye.getTagCompound().getInteger("nodeX" + getWorld(par2EntityPlayer)), eye.getTagCompound().getInteger("nodeY" + getWorld(par2EntityPlayer)), eye.getTagCompound().getInteger("nodeZ" + getWorld(par2EntityPlayer)), par2EntityPlayer);
+                    setCooldown(eye);
+                }
             }
+        } else if(eye.getTagCompound() != null && eye.getTagCompound().hasKey("dimensionID")) {
+    		eye.setTagCompound(null);
+        	if(!world.isRemote)
+        		par2EntityPlayer.sendChatToPlayer(ChatMessageComponent.createFromText(EnumChatFormatting.DARK_RED + "Node dosen't exist!"));
+        	else
+        		par2EntityPlayer.playSound("mob.endermen.death", 1.0f, 1.0f);
         }
-        return ist;
+        return eye;
     }
 
+    private void setCooldown(ItemStack ist) {
+        this.setShort("cooldown", ist, (short) 20);        
+    }
+
+    private void decrementCooldown(ItemStack ist) {
+        if (this.getShort("cooldown", ist) > 0)
+            this.setShort("cooldown", ist, this.getShort("cooldown", ist) - 1);
+    }
+    
     private boolean findAndRemoveEnderPearl(EntityPlayer player) {
         if (player.capabilities.isCreativeMode)
             return true;
@@ -100,41 +139,34 @@ public class ItemWraithEye extends ItemSalamanderEye {
         return false;
     }
 
-    private boolean attemptTeleport(World world, int x, int y, int z,
-            EntityPlayer player) {
-        for (int xD = -2; xD <= 2; xD++) {
-            for (int yD = -1; yD <= 1; yD++) {
-                for (int zD = -2; zD <= 2; zD++) {
-                    if (world.getBlockId(x + xD, y + yD, z + zD) == 0
-                            && world.getBlockId(x + xD, y + yD + 1, z + zD) == 0) {
-                        player.setPositionAndUpdate(x + xD, y + yD, z + zD);
-                        for (int particles = 0; particles < 2; particles++) {
-                            world.spawnParticle("portal", player.posX,
-                                    player.posY, player.posZ,
-                                    world.rand.nextGaussian(),
-                                    world.rand.nextGaussian(),
-                                    world.rand.nextGaussian());
-                        }
-                    }
-                }
-            }
-        }
-        return false;
+    private boolean canTeleport(World world, int x, int y, int z) {
+        if (!world.isAirBlock(x, y + 1, z) || !world.isAirBlock(x, y + 2, z)  )
+            return false;
+        return true;
+    }
+    private void teleportPlayer(World world, int x, int y, int z, EntityPlayer player) {
+        player.setPositionAndUpdate(x, y, z);
+        player.playSound("mob.endermen.portal", 1.0f, 1.0f);
+        for (int particles = 0; particles < 2; particles++) {
+            world.spawnParticle("portal", player.posX, player.posY, player.posZ, world.rand.nextGaussian(), world.rand.nextGaussian(), world.rand.nextGaussian());
+        } 
+        return;
     }
 
     @Override
-    public void addInformation(ItemStack eye, EntityPlayer par2EntityPlayer,
-            List par3List, boolean par4) {
+    public void addInformation(ItemStack eye, EntityPlayer par2EntityPlayer, List par3List, boolean par4) {
         par3List.add("Right clicking a Wraith Node binds to it");
         par3List.add("allowing you to return to it at will,");
         par3List.add("for the cost of an Ender Pearl.");
         String details = EnumChatFormatting.DARK_PURPLE + "Currently bound to ";
-        if (par2EntityPlayer.worldObj.getBlockId(getShort("nodeX" + getWorld(par2EntityPlayer), eye),
-                getShort("nodeY" + getWorld(par2EntityPlayer), eye), getShort("nodeZ" + getWorld(par2EntityPlayer), eye)) != XRBlocks.wraithNode.blockID) {
-            details += "nowhere.";
+    	checkForOldVersions(eye);
+        if (eye.getTagCompound() != null && eye.getTagCompound().getInteger("dimensionID") != Integer.valueOf(getWorld(par2EntityPlayer))) {
+        	details = EnumChatFormatting.DARK_PURPLE + "Out of range!";
+        } else if(eye.getTagCompound() != null && eye.getTagCompound().hasKey("nodeX" + getWorld(par2EntityPlayer)) && eye.getTagCompound().hasKey("nodeY" + getWorld(par2EntityPlayer)) && eye.getTagCompound().hasKey("nodeZ" + getWorld(par2EntityPlayer))) {
+            details += "X: " + eye.getTagCompound().getInteger("nodeX" + getWorld(par2EntityPlayer)) + " Y: "
+                    + eye.getTagCompound().getInteger("nodeY" + getWorld(par2EntityPlayer)) + " Z: " + eye.getTagCompound().getInteger("nodeZ" + getWorld(par2EntityPlayer));
         } else {
-            details += "X: " + getShort("nodeX" + getWorld(par2EntityPlayer), eye) + " Y: "
-                    + getShort("nodeY" + getWorld(par2EntityPlayer), eye) + " Z: " + getShort("nodeZ" + getWorld(par2EntityPlayer), eye);
+        	details += "nowhere.";
         }
         par3List.add(details);
     }
@@ -143,22 +175,53 @@ public class ItemWraithEye extends ItemSalamanderEye {
     public boolean onItemUse(ItemStack ist, EntityPlayer player, World world,
             int x, int y, int z, int side, float xOff, float yOff, float zOff) {
         // if right clicking on a wraith node, bind the eye to that wraith node.
-        if (world.getBlockId(x, y, z) == XRBlocks.wraithNode.blockID) {
-            setWraithNode(ist, x, y, z, player);
+        if ((ist.getTagCompound() == null || !(ist.getTagCompound().hasKey("dimensionID"))) && world.getBlockId(x, y, z) == XRBlocks.wraithNode.blockID) {
+            setWraithNode(ist, x, y, z, Integer.valueOf(getWorld(player)), player);
+            
+            player.playSound("mob.endermen.portal", 1.0f, 1.0f);
+            for (int particles = 0; particles < 12; particles++) {
+                world.spawnParticle("portal", x + world.rand.nextDouble(), y + world.rand.nextDouble(), z + world.rand.nextDouble(), world.rand.nextGaussian(), world.rand.nextGaussian(), world.rand.nextGaussian());
+            } 
+            
             return true;
         } else {
-            onItemRightClick(ist, world, player);
+            // onItemRightClick(ist, world, player);
             return false;
         }
     }
 
-    public void setWraithNode(ItemStack eye, int x, int y, int z, EntityPlayer player) {
-        setShort("nodeX" + getWorld(player), eye, x);
-        setShort("nodeY" + getWorld(player), eye, y);
-        setShort("nodeZ" + getWorld(player), eye, z);
+    public void setWraithNode(ItemStack eye, int x, int y, int z, int dimensionID, EntityPlayer player) {
+        setInteger("nodeX" + getWorld(player), eye, x);
+        setInteger("nodeY" + getWorld(player), eye, y);
+        setInteger("nodeZ" + getWorld(player), eye, z);
+        setInteger("dimensionID", eye, dimensionID);
     }
-    
+
     public String getWorld(EntityPlayer player) {
         return Integer.valueOf(player.worldObj.provider.dimensionId).toString();
+    }
+    
+    private ItemStack checkForOldVersions(ItemStack eye) {
+        if(eye.getTagCompound() != null && !eye.getTagCompound().hasKey("dimensionID")) {
+        	Iterator keys = eye.getTagCompound().getTags().iterator();
+        	NBTTagCompound dummy = new NBTTagCompound();
+        	eye.setTagCompound(new NBTTagCompound());
+        	
+            while(keys.hasNext()) {
+            	NBTBase base = (NBTBase) keys.next();
+            	if(base.getName().contains("nodeX")) {
+            		dummy.setTag("nodeX", base.copy());
+            		eye.getTagCompound().setInteger(base.getName(), (int) dummy.getShort("nodeX"));
+            		eye.getTagCompound().setInteger("dimensionID", Integer.valueOf(base.getName().charAt(base.getName().length() - 1)));
+            	} else if(base.getName().contains("nodeY")) {
+            		dummy.setTag("nodeY", base.copy());
+            		eye.getTagCompound().setInteger(base.getName(), (int) dummy.getShort("nodeY"));
+            	} else if(base.getName().contains("nodeZ")) {
+            		dummy.setTag("nodeZ", base.copy());
+            		eye.getTagCompound().setInteger(base.getName(), (int) dummy.getShort("nodeZ"));
+            	}
+            }
+        }
+        return eye;
     }
 }
