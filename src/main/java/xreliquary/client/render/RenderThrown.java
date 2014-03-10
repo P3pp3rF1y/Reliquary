9a9b74f1595f07809d124fcc9e9680a4009a3d21
x3n0ph0b3 <tfiorentini@exchange.therapymgmt.com>
2014-03-10 14:47:50 -0500

    Tiny fix for thrown item renderer
    magically makes all the thrown [potion] renderers work [I think]. Need
    to take a second look at snowball and verify it still works, and walk
    through each condensed potion and ensure they're all good.

--- src/main/java/xreliquary/client/render/RenderThrown.java
+++ src/main/java/xreliquary/client/render/RenderThrown.java
@@ -18,6 +18,7 @@ public class RenderThrown extends Render {
 
 	@Override
 	public void doRender(Entity par1Entity, double par2, double par4, double par6, float par8, float par9) {
+        this.bindEntityTexture(par1Entity);
 		GL11.glPushMatrix();
 		GL11.glTranslatef((float) par2, (float) par4, (float) par6);
 		GL11.glEnable(GL12.GL_RESCALE_NORMAL);
