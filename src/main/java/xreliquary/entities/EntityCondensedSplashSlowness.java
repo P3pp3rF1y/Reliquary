09a64277778e1c96dbab02fb270a3a47f78660d7
x3n0ph0b3 <x3n0ph0b3@yahoo.com>
2014-03-11 20:38:15 -0500

    Thrown potion refactor
    Mostly code reduction, makes the potion effects slightly easier to find,
    makes the potions share the majority of their particle and bounding box
    effects. Could be better, Living vs Nonliving effects are a bit obtuse -
    the potions as of now only use one or the other.

--- src/main/java/xreliquary/entities/EntityCondensedSplashSlowness.java
+++ src/main/java/xreliquary/entities/EntityCondensedSplashSlowness.java
@@ -19,7 +19,7 @@ import net.minecraft.world.World;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 
-public class EntityCondensedSplashSlowness extends EntityThrowable {
+public class EntityCondensedSplashSlowness extends EntityThrownPotion {
 	public EntityCondensedSplashSlowness(World par1World) {
 		super(par1World);
 	}
@@ -37,72 +37,33 @@ public class EntityCondensedSplashSlowness extends EntityThrowable {
 		super(par1World, par2, par4, par6);
 	}
 
-	/**
-	 * Gets the amount of gravity to apply to the thrown entity with each tick.
-	 */
-	@Override
-	protected float getGravityVelocity() {
-		return 0.03F;
-	}
+    @Override
+    boolean hasLivingEntityEffect() {
+        return true;
+    }
 
-	@Override
-	protected float func_70182_d() {
-		return 0.7F;
-	}
+    @Override
+    void doGroundSplashEffect() {
+        //none
+    }
 
-	@Override
-	protected float func_70183_g() {
-		return -20.0F;
-	}
+    @Override
+    void doLivingSplashEffect(EntityLiving e) {
+        e.addPotionEffect(new PotionEffect(Potion.moveSlowdown.id, 1200, 0));
+    }
 
-	/**
-	 * Called when this EntityThrowable hits a block or entity.
-	 */
-	@Override
-	protected void onImpact(MovingObjectPosition mop) {
-		this.spawnParticles();
-		AxisAlignedBB bb = boundingBox.expand(4.0D, 2.0D, 4.0D);
-		List eList = worldObj.getEntitiesWithinAABB(EntityLiving.class, bb);
-		Iterator i = eList.iterator();
-		while (i.hasNext()) {
-			EntityLiving e = (EntityLiving) i.next();
-			e.addPotionEffect(new PotionEffect(Potion.moveSlowdown.id, 1200, 0));
-		}
+    @Override
+    float getRed() {
+        return 0;
+    }
 
-		this.setDead();
-	}
+    @Override
+    float getGreen() {
+        return 0.4F;
+    }
 
-	private void spawnParticles() {
-		double var8 = posX;
-		double var10 = posY;
-		double var12 = posZ;
-		String var14 = "iconcrack_" + Item.getIdFromItem(Items.potionitem);
-		Random var7 = rand;
-		for (int var15 = 0; var15 < 8; ++var15) {
-			worldObj.spawnParticle(var14, var8, var10, var12, var7.nextGaussian() * 0.15D, var7.nextDouble() * 0.2D, var7.nextGaussian() * 0.15D);
-		}
-
-		float red = 0.0F;
-		float green = 0.4F;
-		float blue = 1.0F;
-		String var19 = "spell";
-
-		for (int var20 = 0; var20 < 100; ++var20) {
-			double var39 = var7.nextDouble() * 4.0D;
-			double var23 = var7.nextDouble() * Math.PI * 2.0D;
-			double var25 = Math.cos(var23) * var39;
-			double var27 = 0.01D + var7.nextDouble() * 0.5D;
-			double var29 = Math.sin(var23) * var39;
-			if (worldObj.isRemote) {
-				EntityFX var31 = Minecraft.getMinecraft().renderGlobal.doSpawnParticle(var19, var8 + var25 * 0.1D, var10 + 0.3D, var12 + var29 * 0.1D, var25, var27, var29);
-				if (var31 != null) {
-					float var32 = 0.75F + var7.nextFloat() * 0.25F;
-					var31.setRBGColorF(red * var32, green * var32, blue * var32);
-					var31.multiplyVelocity((float) var39);
-				}
-			}
-		}
-
-		worldObj.playSoundEffect(posX + 0.5D, posY + 0.5D, posZ + 0.5D, "random.glass", 1.0F, worldObj.rand.nextFloat() * 0.1F + 0.9F);
-	}
+    @Override
+    float getBlue() {
+        return 1.0F;
+    }
 }
--- src/main/java/xreliquary/entities/EntityGlowingWater.java
+++ src/main/java/xreliquary/entities/EntityGlowingWater.java
@@ -2,7 +2,10 @@ package xreliquary.entities;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.EntityFX;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.boss.EntityWither;
@@ -12,6 +15,8 @@ import net.minecraft.entity.monster.EntitySkeleton;
 import net.minecraft.entity.monster.EntityZombie;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityThrowable;
+import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.MovingObjectPosition;
@@ -55,6 +60,11 @@ public class EntityGlowingWater extends EntityThrowable {
 		return -20.0F;
 	}
 
+    private boolean isUndead(Entity mop) {
+        return mop instanceof EntitySkeleton || mop instanceof EntityGhast || mop instanceof EntityWither ||
+                mop instanceof EntityZombie || mop instanceof EntityPigZombie;
+    }
+
 	/**
 	 * Called when this EntityThrowable hits a block or entity.
 	 */
@@ -78,12 +88,36 @@ public class EntityGlowingWater extends EntityThrowable {
 	}
 
 	private void spawnParticles() {
-		for (int particles = 0; particles < 40; particles++) {
-			worldObj.spawnParticle("mobSpellAmbient", posX + rand.nextGaussian() * 4, posY + rand.nextGaussian() * 4, posZ + rand.nextGaussian() * 4, 1.0F, 1.0F, 0.0F);
-		}
-	}
+        double var8 = posX;
+        double var10 = posY;
+        double var12 = posZ;
+        String var14 = "iconcrack_" + Item.getIdFromItem(Items.potionitem);
+        Random var7 = rand;
+        for (int var15 = 0; var15 < 8; ++var15) {
+            worldObj.spawnParticle(var14, var8, var10, var12, var7.nextGaussian() * 0.15D, var7.nextDouble() * 0.2D, var7.nextGaussian() * 0.15D);
+        }
 
-	private boolean isUndead(Entity mop) {
-		return mop instanceof EntitySkeleton || mop instanceof EntityGhast || mop instanceof EntityWither || mop instanceof EntityZombie || mop instanceof EntityPigZombie;
+        float red = 1.0F;
+        float green = 1.0F;
+        float blue = 0.0F;
+        String var19 = "spell";
+
+        for (int var20 = 0; var20 < 100; ++var20) {
+            double var39 = var7.nextDouble() * 4.0D;
+            double var23 = var7.nextDouble() * Math.PI * 2.0D;
+            double var25 = Math.cos(var23) * var39;
+            double var27 = 0.01D + var7.nextDouble() * 0.5D;
+            double var29 = Math.sin(var23) * var39;
+            if (worldObj.isRemote) {
+                EntityFX var31 = Minecraft.getMinecraft().renderGlobal.doSpawnParticle(var19, var8 + var25 * 0.1D, var10 + 0.3D, var12 + var29 * 0.1D, var25, var27, var29);
+                if (var31 != null) {
+                    float var32 = 0.75F + var7.nextFloat() * 0.25F;
+                    var31.setRBGColorF(red * var32, green * var32, blue * var32);
+                    var31.multiplyVelocity((float) var39);
+                }
+            }
+        }
+
+        worldObj.playSoundEffect(posX + 0.5D, posY + 0.5D, posZ + 0.5D, "random.glass", 1.0F, worldObj.rand.nextFloat() * 0.1F + 0.9F);
 	}
 }
