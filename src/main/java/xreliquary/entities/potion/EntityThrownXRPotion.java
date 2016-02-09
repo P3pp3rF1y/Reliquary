cbdb90872e938911232b47ce97c38dc3e63e6d8c
P3pp3rF1y <josef.janecek@gmail.com>
2016-02-09 18:44:12 +0100

    Fixed spawning of particles and sound on splash potion hit

--- src/main/java/xreliquary/entities/potion/EntityThrownXRPotion.java
+++ src/main/java/xreliquary/entities/potion/EntityThrownXRPotion.java
@@ -1,6 +1,8 @@
 package xreliquary.entities.potion;
 
 import io.netty.buffer.ByteBuf;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.particle.EntityFX;
@@ -77,7 +79,6 @@ public class EntityThrownXRPotion extends EntityThrowable implements IEntityAddi
      */
     protected void onImpact(MovingObjectPosition mop)
     {
-        spawnParticles();
         if (!this.worldObj.isRemote)
         {
             List list = essence.getEffects();
@@ -130,7 +131,7 @@ public class EntityThrownXRPotion extends EntityThrowable implements IEntityAddi
                     }
                 }
             }
-
+            spawnParticles();
             this.setDead();
         }
     }
@@ -154,27 +155,52 @@ public class EntityThrownXRPotion extends EntityThrowable implements IEntityAddi
         float green = (((color >> 8) & 255) / 256F);
         float blue = (((color >> 0) & 255) / 256F);
 
-        String var19 = "spell";
-
         for (int var20 = 0; var20 < 100; ++var20) {
             double var39 = var7.nextDouble() * 4.0D;
             double var23 = var7.nextDouble() * Math.PI * 2.0D;
             double var25 = Math.cos(var23) * var39;
             double var27 = 0.01D + var7.nextDouble() * 0.5D;
             double var29 = Math.sin(var23) * var39;
-            if (worldObj.isRemote) {
-                EntityFX var31 = Minecraft.getMinecraft().effectRenderer.spawnEffectParticle(EnumParticleTypes.SPELL.getParticleID(), this.posX + var25 * 0.1D, this.posY + 0.3D, this.posZ + var29 * 0.1D, var25, var27, var29 );
-                if (var31 != null) {
-                    float var32 = 0.75F + var7.nextFloat() * 0.25F;
-                    var31.setRBGColorF(red * var32, green * var32, blue * var32);
-                    var31.multiplyVelocity((float) var39);
-                }
+            EntityFX var31 = spawnEntityFX(Minecraft.getMinecraft(), EnumParticleTypes.SPELL.getParticleID(), EnumParticleTypes.SPELL.getShouldIgnoreRange(), this.posX + var25 * 0.1D, this.posY + 0.3D, this.posZ + var29 * 0.1D, var25, var27, var29, new int[0]);
+            if (var31 != null) {
+                float var32 = 0.75F + var7.nextFloat() * 0.25F;
+                var31.setRBGColorF(red * var32, green * var32, blue * var32);
+                var31.multiplyVelocity((float) var39);
             }
         }
-
-        worldObj.playSoundEffect(posX + 0.5D, posY + 0.5D, posZ + 0.5D, "dig.glass", 1.0F, worldObj.rand.nextFloat() * 0.1F + 0.9F);
+        worldObj.playSoundAtEntity(this, "dig.glass", 1.0F, worldObj.rand.nextFloat() * 0.1F + 0.9F);
     }
 
+    private EntityFX spawnEntityFX(Minecraft mc, int p_174974_1_, boolean ignoreRange, double p_174974_3_, double p_174974_5_, double p_174974_7_, double p_174974_9_, double p_174974_11_, double p_174974_13_, int... p_174974_15_)
+    {
+        if (mc != null && mc.getRenderViewEntity() != null && mc.effectRenderer != null)
+        {
+            int i = mc.gameSettings.particleSetting;
+
+            if (i == 1 && worldObj.rand.nextInt(3) == 0)
+            {
+                i = 2;
+            }
+
+            double d0 = mc.getRenderViewEntity().posX - p_174974_3_;
+            double d1 = mc.getRenderViewEntity().posY - p_174974_5_;
+            double d2 = mc.getRenderViewEntity().posZ - p_174974_7_;
+
+            if (ignoreRange)
+            {
+                return mc.effectRenderer.spawnEffectParticle(p_174974_1_, p_174974_3_, p_174974_5_, p_174974_7_, p_174974_9_, p_174974_11_, p_174974_13_, p_174974_15_);
+            }
+            else
+            {
+                double d3 = 16.0D;
+                return d0 * d0 + d1 * d1 + d2 * d2 > 256.0D ? null : (i > 1 ? null : mc.effectRenderer.spawnEffectParticle(p_174974_1_, p_174974_3_, p_174974_5_, p_174974_7_, p_174974_9_, p_174974_11_, p_174974_13_, p_174974_15_));
+            }
+        }
+        else
+        {
+            return null;
+        }
+    }
     /**
      * (abstract) Protected helper method to read subclass entity data from NBT.
      */
