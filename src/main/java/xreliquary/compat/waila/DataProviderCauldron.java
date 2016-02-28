7ac8ee0d5f23cc696b2f431a8eeedf008bed0447
P3pp3rF1y <josef.janecek@gmail.com>
2016-02-28 11:29:26 +0100

    Added cauldron Waila support

---
+++ src/main/java/xreliquary/compat/waila/DataProviderCauldron.java
@@ -0,0 +1,113 @@
+package xreliquary.compat.waila;
+
+import com.mojang.realmsclient.gui.ChatFormatting;
+import mcp.mobius.waila.api.IWailaConfigHandler;
+import mcp.mobius.waila.api.IWailaDataAccessor;
+import mcp.mobius.waila.api.IWailaDataProvider;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockPos;
+import net.minecraft.util.StatCollector;
+import net.minecraft.world.World;
+import xreliquary.blocks.BlockApothecaryCauldron;
+import xreliquary.blocks.tile.TileEntityCauldron;
+import xreliquary.init.ModBlocks;
+import xreliquary.util.potions.PotionEssence;
+import xreliquary.util.potions.XRPotionHelper;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class DataProviderCauldron implements IWailaDataProvider {
+    @Override
+    public ItemStack getWailaStack(IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        return new ItemStack(ModBlocks.apothecaryCauldron);
+    }
+
+    @Override
+    public List<String> getWailaHead(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        return null;
+    }
+
+    @Override
+    public List<String> getWailaBody(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        if (! (accessor.getBlock() instanceof BlockApothecaryCauldron && accessor.getTileEntity() instanceof TileEntityCauldron))
+            return currenttip;
+
+        TileEntityCauldron cauldron = (TileEntityCauldron) accessor.getTileEntity();
+
+        if (cauldron == null || cauldron.potionEssence == null)
+            return currenttip;
+
+        if (!cauldron.hasNetherwart)
+            currenttip.add(ChatFormatting.RED + StatCollector.translateToLocal("waila.xreliquary.cauldron.missing_netherwart") + ChatFormatting.RESET);
+
+        StringBuilder ingredients = new StringBuilder();
+        if (cauldron.redstoneCount > 0){
+            ingredients.append(cauldron.redstoneCount);
+            ingredients.append("x ");
+            ingredients.append(Items.redstone.getItemStackDisplayName(new ItemStack(Items.redstone)));
+            ingredients.append(" ");
+        }
+
+        if (cauldron.glowstoneCount > 0){
+            ingredients.append(cauldron.glowstoneCount);
+            ingredients.append("x ");
+            ingredients.append(Items.glowstone_dust.getItemStackDisplayName(new ItemStack(Items.glowstone_dust)));
+        }
+
+        currenttip.add(ingredients.toString());
+
+        if (cauldron.hasGunpowder) {
+            currenttip.add(ChatFormatting.WHITE.toString() + cauldron.getLiquidLevel() + "x" + StatCollector.translateToLocal("waila.xreliquary.cauldron.splash") + ChatFormatting.RESET);
+        } else {
+            currenttip.add(ChatFormatting.WHITE.toString() + cauldron.getLiquidLevel() + "x" + StatCollector.translateToLocal("waila.xreliquary.cauldron.potion") + ChatFormatting.RESET);
+        }
+
+        addEffectsInfo(currenttip, cauldron.potionEssence);
+
+        return currenttip;
+    }
+
+    private void addEffectsInfo(List<String> currenttip, PotionEssence essence) {
+        List<String> effectLines = new ArrayList<>();
+
+        XRPotionHelper.addPotionInfo(essence, effectLines, false);
+
+        StringBuilder sb = new StringBuilder();
+        int effectsInLine = 0;
+        Iterator<String> iterator = effectLines.iterator();
+        while (iterator.hasNext()) {
+            effectsInLine++;
+            String line = iterator.next();
+            sb.append(line);
+
+            //display 2 effects per line
+            if (effectsInLine == 2) {
+                effectsInLine = 0;
+                currenttip.add(sb.toString());
+                sb = new StringBuilder();
+                continue;
+            }
+            if (iterator.hasNext())
+                sb.append(", ");
+
+        }
+        if(sb.length() > 0)
+            currenttip.add(sb.toString());
+    }
+
+    @Override
+    public List<String> getWailaTail(ItemStack itemStack, List<String> currenttip, IWailaDataAccessor accessor, IWailaConfigHandler config) {
+        return null;
+    }
+
+    @Override
+    public NBTTagCompound getNBTData(EntityPlayerMP player, TileEntity te, NBTTagCompound tag, World world, BlockPos pos) {
+        return null;
+    }
+}
--- src/main/java/xreliquary/compat/waila/WailaCallbackHandler.java
+++ src/main/java/xreliquary/compat/waila/WailaCallbackHandler.java
@@ -1,10 +1,13 @@
 package xreliquary.compat.waila;
 
 import mcp.mobius.waila.api.IWailaRegistrar;
+import xreliquary.blocks.BlockApothecaryCauldron;
 import xreliquary.blocks.BlockApothecaryMortar;
 
 public class WailaCallbackHandler {
     public static void callbackRegister(IWailaRegistrar registrar) {
         registrar.registerBodyProvider(new DataProviderMortar(), BlockApothecaryMortar.class);
+        registrar.registerBodyProvider(new DataProviderCauldron(), BlockApothecaryCauldron.class);
+        registrar.registerStackProvider(new DataProviderCauldron(), BlockApothecaryCauldron.class);
     }
 }
