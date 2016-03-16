65c27394f669bcd01c8462558d8d8bdcc622f192
P3pp3rF1y <p3pp3rf1y@gmail.com>
2016-03-16 14:47:16 +0100

    Added flight potion icon

--- src/main/java/xreliquary/util/potions/PotionFlight.java
+++ src/main/java/xreliquary/util/potions/PotionFlight.java
@@ -1,12 +1,28 @@
 package xreliquary.util.potions;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
 import xreliquary.reference.Reference;
 import xreliquary.reference.Settings;
 
 public class PotionFlight extends Potion {
+	private ResourceLocation icon = new ResourceLocation(Reference.MOD_ID, "textures/gui/flight_effect.png");
+
 	public PotionFlight() {
 		super(new ResourceLocation(Reference.DOMAIN + "potions/flight"), false, 0);
+	}
+
+	@Override
+	public void renderInventoryEffect(int x, int y, PotionEffect effect, Minecraft mc) {
+		super.renderInventoryEffect(x, y, effect, mc);
+
+		mc.renderEngine.bindTexture(icon);
+
+		GlStateManager.enableBlend();
+		Gui.drawModalRectWithCustomSizedTexture(x + 6, y + 7, 0, 0, 16, 16, 16, 16);
 	}
 }
