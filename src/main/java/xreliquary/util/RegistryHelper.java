b0bcff98a6b3e2fc72d8b3056940976047cfa09d
P3pp3rF1y <p3pp3rf1y@gmail.com>
2016-04-09 23:26:46 +0200

    Fix for messy mods not registering items

--- src/main/java/xreliquary/util/RegistryHelper.java
+++ src/main/java/xreliquary/util/RegistryHelper.java
@@ -14,7 +14,12 @@ public class RegistryHelper
 	}
 
 	public static String getItemRegistryName(Item item) {
-		return GameData.getItemRegistry().getNameForObject(item).toString();
+		ResourceLocation rl = GameData.getItemRegistry().getNameForObject(item);
+		//null check because some mods don't properly register items they use in recipes
+		if (rl != null)
+			return rl.toString();
+
+		return "";
 	}
 
 	public static Block getBlockFromName(String registryName) {
