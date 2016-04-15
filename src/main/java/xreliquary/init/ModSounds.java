e72df7651b566476aae3d6d6748dcdd622baa980
P3pp3rF1y <p3pp3rf1y@gmail.com>
2016-04-15 15:54:31 +0200

    Fixed sound registration

--- src/main/java/xreliquary/init/ModSounds.java
+++ src/main/java/xreliquary/init/ModSounds.java
@@ -1,13 +1,24 @@
 package xreliquary.init;
 
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SoundEvent;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+import xreliquary.reference.Reference;
 
 public class ModSounds {
-	public static SoundEvent book;
-	public static SoundEvent xload;
-	public static SoundEvent xshot;
+	public static SoundEvent book = new SoundEvent(new ResourceLocation(Reference.MOD_ID, "book"));
+	public static SoundEvent xload = new SoundEvent(new ResourceLocation(Reference.MOD_ID, "xload"));
+	public static SoundEvent xshot = new SoundEvent(new ResourceLocation(Reference.MOD_ID, "xshot"));
 
-	public void init() {
+	public static void init() {
+		registerSoundEvent(book);
+		registerSoundEvent(xload);
+		registerSoundEvent(xshot);
+
 		//TODO fix sounds when forge ready
+	}
+
+	private static void registerSoundEvent(SoundEvent soundEvent) {
+		GameRegistry.register(soundEvent.setRegistryName(soundEvent.getSoundName()));
 	}
 }
