fd43d0a0f2e6a0c21825f20ae05592a67bf7f6bc
P3pp3rF1y <p3pp3rf1y@gmail.com>
2016-08-11 22:57:42 +0200

    removing most of JER compatibility code as this is now done in JER itself

--- src/main/java/xreliquary/compat/jer/JERCompat.java
+++ src/main/java/xreliquary/compat/jer/JERCompat.java
@@ -39,44 +39,6 @@ public class JERCompat implements ICompat {
 	public static IJERAPI api;
 
 	public static void register(World world) {
-		//Squid
-		registerMobDrop(EntitySquid.class, XRRecipes.ingredient(1, Reference.SQUID_INGREDIENT_META), Settings.MobDrops.getBaseDrop(Names.squid_beak), Conditional.playerKill);
-
-		//Witch
-		registerMobDrop(EntityWitch.class, new ItemStack(ModItems.witchHat), Settings.MobDrops.getBaseDrop(Names.witch_hat), Conditional.playerKill);
-
-		//Spider
-		registerMobDrop(EntitySpider.class, XRRecipes.ingredient(1, Reference.SPIDER_INGREDIENT_META), Settings.MobDrops.getBaseDrop(Names.spider_fangs), Conditional.playerKill);
-
-/*
-      TODO fix when there is proper way to handle this in JER
-		//Skeleton
-		registerMobDrop(EntitySkeleton.class, WatchableData.REGULAR_SKELETON, XRRecipes.ingredient(1, Reference.SKELETON_INGREDIENT_META), Settings.MobDrops.getBaseDrop(Names.rib_bone), Conditional.playerKill);
-
-		//Wither Skeleton
-		registerMobDrop(EntitySkeleton.class, WatchableData.WITHER_SKELETON, XRRecipes.ingredient(1, Reference.WITHER_INGREDIENT_META), Settings.MobDrops.getBaseDrop(Names.withered_rib), Conditional.playerKill);
-*/
-
-
-
-		//Zombie
-		registerMobDrop(EntityZombie.class, XRRecipes.ingredient(1, Reference.ZOMBIE_INGREDIENT_META), Settings.MobDrops.getBaseDrop(Names.zombie_heart), Conditional.playerKill);
-
-		//Slime
-		registerMobDrop(EntitySlime.class, XRRecipes.ingredient(1, Reference.SLIME_INGREDIENT_META), Settings.MobDrops.getBaseDrop(Names.slime_pearl), Conditional.playerKill);
-
-		//Blaze
-		registerMobDrop(EntityBlaze.class, XRRecipes.ingredient(1, Reference.MOLTEN_INGREDIENT_META), Settings.MobDrops.getBaseDrop(Names.blaze_molten_core), Conditional.playerKill);
-
-		//Magma Cube
-		registerMobDrop(EntityMagmaCube.class, XRRecipes.ingredient(1, Reference.MOLTEN_INGREDIENT_META), Settings.MobDrops.getBaseDrop(Names.magma_cube_molten_core), Conditional.playerKill);
-
-		//Ghast
-		registerMobDrop(EntityGhast.class, XRRecipes.ingredient(1, Reference.CREEPER_INGREDIENT_META), Settings.MobDrops.getBaseDrop(Names.ghast_gland), Conditional.playerKill);
-
-		//Creeper
-		registerMobDrop(EntityCreeper.class, XRRecipes.ingredient(1, Reference.CREEPER_INGREDIENT_META), Settings.MobDrops.getBaseDrop(Names.creeper_gland), Conditional.playerKill);
-
 /*
  TODO fix when there are loot tables or JER logic
 		//Charged Creeper
@@ -85,21 +47,6 @@ public class JERCompat implements ICompat {
 		chargedCreeper.getDataManager().updateObject(17, Byte.valueOf((byte)1));
 		api.getMobRegistry().register(chargedCreeper, LightLevel.hostile, 5, eyeOfTheStorm);
 */
-
-
-
-		//Enderman
-		registerMobDrop(EntityEnderman.class, XRRecipes.ingredient(1, Reference.ENDER_INGREDIENT_META), Settings.MobDrops.getBaseDrop(Names.ender_heart), Conditional.playerKill);
-
-		//Bat
-		registerMobDrop(EntityBat.class, XRRecipes.ingredient(1, Reference.BAT_INGREDIENT_META), Settings.MobDrops.getBaseDrop(Names.bat_wing), Conditional.playerKill);
-
-		//Snow Golem
-		registerMobDrop(EntitySnowman.class, XRRecipes.ingredient(1, Reference.FROZEN_INGREDIENT_META), Settings.MobDrops.getBaseDrop(Names.frozen_core), Conditional.playerKill);
-
-		//Guardian
-		registerMobDrop(EntityGuardian.class, XRRecipes.ingredient(1, Reference.GUARDIAN_INGREDIENT_META), 0.1f, Conditional.playerKill);
-
 	}
 
 	private static void registerMobDrop(Class<? extends EntityLivingBase> entity, WatchableData watchableData, ItemStack drop, float chance, Conditional... conditionals) {
