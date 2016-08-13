f14f8aad0e92bf766ce7f75104a90ce5cf86b5ae
P3pp3rF1y <p3pp3rf1y@gmail.com>
2016-08-13 20:52:40 +0200

    Added JER condition for powered property and got rid of previous JER compatibility stuff

--- src/main/java/xreliquary/loot/EntityPowered.java
+++ src/main/java/xreliquary/loot/EntityPowered.java
@@ -4,15 +4,20 @@ import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
+import jeresources.api.conditionals.Conditional;
+import jeresources.api.conditionals.ICustomEntityProperty;
+import jeresources.api.drop.LootDrop;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.monster.EntityCreeper;
 import net.minecraft.util.JsonUtils;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.storage.loot.properties.EntityProperty;
-
+import net.minecraftforge.fml.common.Optional;
+import xreliquary.reference.Compatibility;
 import java.util.Random;
 
-public class EntityPowered implements EntityProperty {
+@Optional.Interface(iface = "jeresources.api.conditionals.ICustomEntityProperty", modid = Compatibility.MOD_ID.JER, striprefs = true)
+public class EntityPowered implements EntityProperty, ICustomEntityProperty {
 
 	private final boolean powered;
 
@@ -28,6 +33,12 @@ public class EntityPowered implements EntityProperty {
 		EntityCreeper creeper = (EntityCreeper) entityIn;
 
 		return creeper.getPowered();
+	}
+
+	@Override
+	@Optional.Method(modid = Compatibility.MOD_ID.JER)
+	public void applyProperty(LootDrop lootDrop) {
+		lootDrop.addConditional(Conditional.powered);
 	}
 
 	public static class Serializer extends EntityProperty.Serializer<EntityPowered> {
