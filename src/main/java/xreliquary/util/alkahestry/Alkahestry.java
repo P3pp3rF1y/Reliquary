956afb957081c4e0abe05e79126dffcf3f9bac32
P3pp3rF1y <p3pp3rf1y@gmail.com>
2016-09-08 18:01:25 +0200

    Fixed using alkahestry tome for ore dictionary items. Fixes #160

--- src/main/java/xreliquary/util/alkahestry/Alkahestry.java
+++ src/main/java/xreliquary/util/alkahestry/Alkahestry.java
@@ -9,7 +9,7 @@ public class Alkahestry {
 	public static AlkahestCraftRecipe getDictionaryKey(ItemStack stack) {
 		for(AlkahestCraftRecipe recipe : Settings.AlkahestryTome.craftingRecipes.values()) {
 			if(recipe.dictionaryName == null)
-				return null;
+				continue;
 			for(ItemStack dict : OreDictionary.getOres(recipe.dictionaryName)) {
 				if(OreDictionary.itemMatches(dict, stack, false))
 					return recipe;
