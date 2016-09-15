593cb442ab3654563a77fd8c5167e98f8b23c776
P3pp3rF1y <p3pp3rf1y@gmail.com>
2016-09-15 15:41:36 +0200

    Refactorred flight potion to properly use potion logic instead of PlayerTick event

--- src/main/java/xreliquary/util/potions/PotionFlight.java
+++ src/main/java/xreliquary/util/potions/PotionFlight.java
@@ -3,9 +3,15 @@ package xreliquary.util.potions;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.ai.attributes.AbstractAttributeMap;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.network.play.server.SPacketPlayerAbilities;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
 import net.minecraft.util.ResourceLocation;
+import xreliquary.init.ModPotions;
 import xreliquary.reference.Reference;
 
 public class PotionFlight extends Potion {
@@ -31,6 +37,44 @@ public class PotionFlight extends Potion {
 	@Override
 	public void renderInventoryEffect(int x, int y, PotionEffect effect, Minecraft mc) {
 		render(mc, x + 6, y + 7);
+	}
+
+	@Override
+	public boolean isReady(int duration, int amplifier) {
+		return true;
+	}
+
+	@Override
+	public void performEffect(EntityLivingBase entityLivingBase, int p_76394_2_) {
+		if (entityLivingBase.worldObj.isRemote || !(entityLivingBase instanceof EntityPlayer))
+			return;
+
+		EntityPlayer player = (EntityPlayer) entityLivingBase;
+
+		if (!player.capabilities.allowFlying) {
+			player.capabilities.allowFlying = true;
+			((EntityPlayerMP) player).connection.sendPacket(new SPacketPlayerAbilities(player.capabilities));
+		}
+		player.fallDistance = 0;
+	}
+
+	@Override
+	public void removeAttributesModifiersFromEntity(EntityLivingBase entityLivingBase, AbstractAttributeMap attributeMap, int amplifier) {
+		super.removeAttributesModifiersFromEntity(entityLivingBase, attributeMap, amplifier);
+
+		if (!(entityLivingBase instanceof EntityPlayer))
+			return;
+
+		EntityPlayer player = (EntityPlayer) entityLivingBase;
+
+		if (player.getActivePotionEffect(ModPotions.potionFlight) != null)
+			return;
+
+		if (!player.capabilities.isCreativeMode) {
+			player.capabilities.allowFlying = false;
+			player.capabilities.isFlying = false;
+			((EntityPlayerMP) player).connection.sendPacket(new SPacketPlayerAbilities(player.capabilities));
+		}
 	}
 
 	private void render(Minecraft mc, int x, int y) {
