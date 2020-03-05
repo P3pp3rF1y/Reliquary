55ac1bcc3383767d3e597083e3f302786a6c53d1
P3pp3rF1y <p3pp3rf1y@gmail.com>
2020-03-05 12:04:47 +0100

    Fixes void tear tooltip

--- src/main/java/xreliquary/items/VoidTearItem.java
+++ src/main/java/xreliquary/items/VoidTearItem.java
@@ -22,8 +22,8 @@ import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.RayTraceContext;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.util.text.TextFormatting;
-import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
@@ -110,7 +110,7 @@ public class VoidTearItem extends ToggleableItem {
 
 		if (isEnabled(voidTear)) {
 			LanguageHelper.formatTooltip(TOOLTIP_PREFIX + Reference.MOD_ID + ".absorb_active", ImmutableMap.of("item", TextFormatting.YELLOW + contents.getDisplayName().getString()), tooltip);
-			tooltip.add(new TranslationTextComponent(TOOLTIP_PREFIX + Reference.MOD_ID + ".absorb_tear"));
+			tooltip.add(new StringTextComponent(LanguageHelper.getLocalization(TOOLTIP_PREFIX + Reference.MOD_ID + ".absorb_tear")));
 		}
 		LanguageHelper.formatTooltip(TOOLTIP_PREFIX + Reference.MOD_ID + ".tear_quantity", ImmutableMap.of("item", contents.getDisplayName().getString(), "amount", Integer.toString(contents.getCount())), tooltip);
 	}
