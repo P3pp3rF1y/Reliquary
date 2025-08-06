ee6d1e36b96f517fd08e7c598c801e24dccf9f8a
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-08-06 12:33:05 +0200

    fix: 🐛 Fixed missing sound when drinking Reliquary potions

--- src/main/java/reliquary/item/PotionItem.java
+++ src/main/java/reliquary/item/PotionItem.java
@@ -1,21 +1,19 @@
 package reliquary.item;
 
 import net.minecraft.core.component.DataComponents;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.ItemUseAnimation;
-import net.minecraft.world.item.ItemUtils;
 import net.minecraft.world.item.alchemy.PotionContents;
+import net.minecraft.world.item.component.Consumables;
 import net.minecraft.world.level.Level;
 import reliquary.init.ModItems;
 import reliquary.util.potions.PotionHelper;
 
 public class PotionItem extends PotionItemBase {
 	public PotionItem(Properties properties) {
-		super(properties);
+		super(properties.component(DataComponents.CONSUMABLE, Consumables.DEFAULT_DRINK).usingConvertsTo(ModItems.EMPTY_POTION_VIAL.get()));
 	}
 
 	@Override
@@ -29,31 +27,11 @@ public class PotionItem extends PotionItemBase {
 	}
 
 	@Override
-	public InteractionResult use(Level level, Player player, InteractionHand hand) {
-		ItemStack stack = player.getItemInHand(hand);
-		if (PotionHelper.hasPotionContents(stack)) {
-			return ItemUtils.startUsingInstantly(level, player, hand);
-		} else {
-			return InteractionResult.PASS;
-		}
-	}
-
-	@Override
 	public ItemStack finishUsingItem(ItemStack stack, Level level, LivingEntity entity) {
-		if (!(entity instanceof Player player) || level.isClientSide) {
-			return stack;
-		}
+		ItemStack result = super.finishUsingItem(stack, level, entity);
 
-		PotionHelper.applyEffectsToEntity(stack.getOrDefault(DataComponents.POTION_CONTENTS, PotionContents.EMPTY), player, null, player);
-
-		if (!player.isCreative()) {
-			stack.shrink(1);
-			ItemStack emptyVial = new ItemStack(ModItems.EMPTY_POTION_VIAL.get());
-			if (stack.getCount() <= 0) {
-				return emptyVial;
-			} else {
-				player.getInventory().add(emptyVial);
-			}
+		if ((entity instanceof Player player) && !level.isClientSide) {
+			PotionHelper.applyEffectsToEntity(stack.getOrDefault(DataComponents.POTION_CONTENTS, PotionContents.EMPTY), player, null, player);
 		}
 
 		return stack;
