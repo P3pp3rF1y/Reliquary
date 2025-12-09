88cb0f50323097e59a7a587f0ede7eff01d75053
P3pp3rF1y <p3pp3rf1y@gmail.com>
2025-12-09 22:48:44 +0100

    fix: 🐛 Fixed charging of Alkahestry Tome from player's inventory which would sometimes give it more charge than it should based on item consumed

--- src/main/java/reliquary/item/AlkahestryTomeItem.java
+++ src/main/java/reliquary/item/AlkahestryTomeItem.java
@@ -67,7 +67,7 @@ public class AlkahestryTomeItem extends ChargeableItem {
 		}
 
 		for (AlkahestryChargingRecipe recipe : AlkahestryRecipeRegistry.getChargingRecipes()) {
-			consumeAndCharge(tome, 0, player, getChargeLimit() - getCharge(tome), recipe.getChargeToAdd(), 16);
+			consumeAndCharge(tome, 0, player, getChargeLimit() - getCharge(tome), recipe.getChargeToAdd(), 16, recipe.getChargingIngredient());
 		}
 	}
 
