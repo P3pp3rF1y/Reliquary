9883431d9480d77acc44de161f6ef1bb357e5aa0
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-04-21 22:16:41 +0200

    feat: ✨ Added Clearable support to item-holding block entities

--- src/main/java/reliquary/block/tile/ApothecaryMortarBlockEntity.java
+++ src/main/java/reliquary/block/tile/ApothecaryMortarBlockEntity.java
@@ -5,6 +5,7 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.core.NonNullList;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.Clearable;
 import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.alchemy.PotionContents;
@@ -23,7 +24,7 @@ import reliquary.util.potions.PotionIngredient;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ApothecaryMortarBlockEntity extends BlockEntityBase implements IJadeDataChangeIndicator {
+public class ApothecaryMortarBlockEntity extends BlockEntityBase implements IJadeDataChangeIndicator, Clearable {
 	public static final int PESTLE_USAGE_MAX = 5; // the number of times you have to use the pestle
 	// counts the number of times the player has right clicked the block
 	// arbitrarily setting the number of times the player needs to grind the
@@ -164,6 +165,20 @@ public class ApothecaryMortarBlockEntity extends BlockEntityBase implements IJad
 
 	public IItemHandler getItems() {
 		return items;
+	}
+
+	@Override
+	public void clearContent() {
+		for (int slot = 0; slot < items.getSlots(); ++slot) {
+			items.setStackInSlot(slot, ItemStack.EMPTY);
+		}
+		pestleUsedCounter = 0;
+		finishCoolDown = 0;
+		dataChanged = true;
+		setChanged();
+		if (level != null) {
+			WorldHelper.notifyBlockUpdate(this);
+		}
 	}
 
 	public void dropItems(Level level) {
