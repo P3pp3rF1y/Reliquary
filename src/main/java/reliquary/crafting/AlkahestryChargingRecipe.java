3e3b1a38c5d7fe8d45b3804a8d29a5e23f4f4b41
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-06-07 20:09:33 +0200

    fix: 🐛 Fixed duplicate and broken Alkahestry recipes in JEI view

--- src/main/java/reliquary/crafting/AlkahestryChargingRecipe.java
+++ src/main/java/reliquary/crafting/AlkahestryChargingRecipe.java
@@ -126,7 +126,7 @@ public class AlkahestryChargingRecipe implements CraftingRecipe {
 						)
 						.apply(instance, AlkahestryChargingRecipe::new));
 		private static final StreamCodec<RegistryFriendlyByteBuf, AlkahestryChargingRecipe> STREAM_CODEC = StreamCodec.composite(
-				Ingredient.CONTENTS_STREAM_CODEC,
+				ByteBufCodecs.fromCodecWithRegistries(Ingredient.CODEC),
 				AlkahestryChargingRecipe::getChargingIngredient,
 				ByteBufCodecs.INT,
 				AlkahestryChargingRecipe::getChargeToAdd,
