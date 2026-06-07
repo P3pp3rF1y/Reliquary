f1094eafe25c8b87f4adaa5ef98179fa63cb3dc7
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-06-07 20:11:12 +0200

    fix: 🐛 Fixed duplicate and broken Alkahestry recipes in JEI view

--- src/main/java/reliquary/crafting/InfernalTearValueRecipe.java
+++ src/main/java/reliquary/crafting/InfernalTearValueRecipe.java
@@ -84,7 +84,7 @@ public class InfernalTearValueRecipe implements Recipe<RecipeInput> {
 				)
 				.apply(instance, InfernalTearValueRecipe::new));
 		private static final StreamCodec<RegistryFriendlyByteBuf, InfernalTearValueRecipe> STREAM_CODEC = StreamCodec.composite(
-				Ingredient.CONTENTS_STREAM_CODEC,
+				ByteBufCodecs.fromCodecWithRegistries(Ingredient.CODEC),
 				InfernalTearValueRecipe::getIngredient,
 				ByteBufCodecs.INT,
 				InfernalTearValueRecipe::getExperiencePoints,
