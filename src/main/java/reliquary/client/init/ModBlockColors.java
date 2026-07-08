acfdd0fec0b7650be1afa2636ce565ca79229ac5
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-07-08 07:58:22 +0200

    fix: 🐛 Fixed Apothecary Cauldron sprint particles crashing near block entities

--- src/main/java/reliquary/client/init/ModBlockColors.java
+++ src/main/java/reliquary/client/init/ModBlockColors.java
@@ -10,11 +10,8 @@ public class ModBlockColors {
 
 	public static void registerBlockColors(RegisterColorHandlersEvent.Block event) {
 		event.register((state, level, pos, tintIndex) -> {
-			if (level != null && pos != null) {
-				ApothecaryCauldronBlockEntity cauldron = (ApothecaryCauldronBlockEntity) level.getBlockEntity(pos);
-				if (cauldron != null && cauldron.getLiquidLevel() > 0) {
-					return cauldron.getColorMultiplier();
-				}
+			if (level != null && pos != null && level.getBlockEntity(pos) instanceof ApothecaryCauldronBlockEntity cauldron && cauldron.getLiquidLevel() > 0) {
+				return cauldron.getColorMultiplier();
 			}
 
 			return -1;
