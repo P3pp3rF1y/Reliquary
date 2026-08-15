4014eb6a72a36264860b0665a9d9229d67b14f87
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-08-15 23:44:44 +0200

    fix: 🐛 Fixed unsafe network packet handling

--- src/main/java/reliquary/item/ChargeableItem.java
+++ src/main/java/reliquary/item/ChargeableItem.java
@@ -52,8 +52,8 @@ public abstract class ChargeableItem extends ToggleableItem {
 
 	public static final Codec<Map<Integer, Integer>> PARTIAL_CHARGES_CODEC = Codec.unboundedMap(STRING_ENCODED_INT, ExtraCodecs.NON_NEGATIVE_INT);
 
-	public static final StreamCodec<FriendlyByteBuf, Map<Integer, Integer>> PARTIAL_CHARGES_STREAM_CODEC = StreamCodec
-			.of((buf, map) -> buf.writeMap(map, ByteBufCodecs.INT, ByteBufCodecs.INT), buf -> buf.readMap(ByteBufCodecs.INT, ByteBufCodecs.INT));
+	public static final StreamCodec<FriendlyByteBuf, Map<Integer, Integer>> PARTIAL_CHARGES_STREAM_CODEC = ByteBufCodecs.map(HashMap::new, ByteBufCodecs.INT,
+			ByteBufCodecs.INT, 64);
 
 	protected static final int FIRST_SLOT = 0;
 
