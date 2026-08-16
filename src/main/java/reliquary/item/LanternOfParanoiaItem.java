472723b513b2cc21756c4f09a08a7a122d0661f9
P3pp3rF1y <p3pp3rf1y@gmail.com>
2026-08-16 12:31:51 +0200

    fix: 🐛 Fixed Void Tear duplicating items from protected inventory slots

--- src/main/java/reliquary/item/LanternOfParanoiaItem.java
+++ src/main/java/reliquary/item/LanternOfParanoiaItem.java
@@ -141,7 +141,9 @@ public class LanternOfParanoiaItem extends ToggleableItem {
 			for (Direction side : trySides) {
 				BlockState torchBlockState = getTorchSideAttempt(player, torch, pos, side);
 				if (torchBlockState == null || !torchBlockState.canSurvive(level, pos) || !level.isUnobstructed(torchBlockState, pos, CollisionContext.empty())
-						|| !(InventoryHelper.consumeItem(torchStack, player, 0, 1) || findAndDrainSojournersStaff(player))) {
+						|| !(player.isCreative() || InventoryHelper
+								.consumeItemStack(candidate -> ItemStack.isSameItemSameComponents(torchStack, candidate), player, 1).getCount() == 1
+								|| findAndDrainSojournersStaff(player))) {
 					continue;
 				}
 
